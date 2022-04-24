04d00b2bfcac3f9406e7c33323c6c9619f70e6d1
최진흥 <jinheung90@gmail.com>
2022-04-24 20:38:19 +0900

    deploy works

--- shop-payment/src/main/java/com/jinheung/JinheungApplication.java
+++ shop-payment/src/main/java/com/jinheung/JinheungApplication.java
@@ -1,8 +1,7 @@
-package com.jinheung.project;
+package com.jinheung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
