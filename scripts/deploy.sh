8f14aa22b042b5ae230d5ec8d40cc04b07896ebf
최진흥 <jinheung90@gmail.com>
2022-06-01 21:38:58 +0900

    gateway server upload

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,49 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
+BUILD_PATH=$(ls /opt/app/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_PATH)
+echo "> build 파일명: $JAR_NAME"
 
-PROFILE_FILE="/opt/yaml/profile.txt"
+echo "> build 파일 복사"
+DEPLOY_PATH=/home/ec2-user/
+cp $BUILD_PATH $DEPLOY_PATH
+
+echo "> springboot-deploy.jar 교체"
+CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
 APPLICATION_JAR_NAME=springboot-deploy.jar
-APP_JAR_NEW=audivice-signal-main-1.0.1.jar
-DEPLOY_PATH="/home/ec2-user/"
-BUILD_DIR="/opt/app/user/jinhueng-api-gateway/build/libs/" # 바뀐 지점
-BUILD_FILEPATH=$BUILD_DIR$APP_JAR_NEW
 APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
-STDOUT=/logs/user/stdout.log
-STDERR=/logs/user/stderr.log
 
+ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
 
-if [ ! -d "/opt/yaml" ]; then
-  sudo mkdir /opt
-  sudo mkdir /opt/yaml
+echo "> 현재 실행중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> sudo kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
+  sleep 5
 fi
 
-if [ ! -d "/logs/gateway" ]; then
-  sudo mkdir /logs
-  sudo mkdir /logs/client
-  sudo mkdir /logs/gateway
-  sudo mkdir /logs/eureka
+sudo fuser -k 8081/tcp
 
-fi
-
-sudo chmod -R 777 /opt/yaml/
-sudo chmod -R 777 /logs
-# Code deploy can not inject environment variables
-# So, profile should be checked in deploy script
-# test server is only one and develop deploy is changed
-if [ "$APPLICATION_NAME" == "jinheung-project" ]; then
-  sudo fuser -k 8081/tcp
-  sudo fuser -k 8082/tcp
-  sudo fuser -k 8084/tcp
-  ACTIVE_PROFILE=prod
-  BUILD_DIR="/opt/app/gateway/jinhueng-api-gateway/build/libs/"
-  SPRING_OPTIONS="-Dspring.profiles.active=$ACTIVE_PROFILE -Dserver.port=8081"
-  sudo nohup java -jar $SPRING_OPTIONS $APPLICATION_JAR 1>>$STDOUT 2>>$STDERR &
-
-  BUILD_DIR="/opt/app/client/jinhueng-eureka-server/build/libs/"
-  SPRING_OPTIONS="-Dspring.profiles.active=$ACTIVE_PROFILE -Dserver.port=8082"
-  sudo nohup java -jar $SPRING_OPTIONS $APPLICATION_JAR 1>>$STDOUT 2>>$STDERR &
-
-  BUILD_DIR="/opt/app/shop-main/jinhueng-eureka-server/build/libs/"
-  SPRING_OPTIONS="-Dspring.profiles.active=$ACTIVE_PROFILE -Dserver.port=8084"
-  sudo nohup java -jar $SPRING_OPTIONS $APPLICATION_JAR 1>>$STDOUT 2>>$STDERR &
-
-fi
+echo "> $APPLICATION_JAR 배포"
+sudo nohup java -jar \
+    -Dspring.config.location=/opt/yaml/application-real.yaml \
+    -Dspring.profiles.active=real \
+    $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
